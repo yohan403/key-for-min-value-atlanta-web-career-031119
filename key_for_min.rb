736594dcd01faef61b083ec96678474ab125230e
John Cho <johnycho16@gmail.com>
2019-03-07 05:37:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,7 +16,7 @@ def key_for_min_value(name_hash)
       if nest[1] <= initial_val
         initial_val = nest[1]
         least_key = nest[0]
-        puts least_key
+        puts least_key.inspect
       end
     end
   end
